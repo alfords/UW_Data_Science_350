8b4f50ad1901000dd9734b05f8278d52c64d783a
A.Kramer <xxxxx@gmail.com>
2015-07-20 23:32:12 -0700

    Added simple linear models

--- Week5/homework/A.Kramer-Week5-Homework.R
+++ Week5/homework/A.Kramer-Week5-Homework.R
@@ -4,22 +4,46 @@
 ## Week 5 Homework (Lecture 4)
 ## ---------------------------------------------------
 
-# Set working directory
-setwd('/Users/voitel/TRAINING/UW_Data_Science/UW_Data_Science_350/Week5/homework')
+simple_lm <- function(x, y, title) {
+    plot(x, y, pch = 16, main = title)
+    best_fit_line <- lm(y ~ x)
+    abline(best_fit_line)
+}
 
-# Import libraries
-require(logging)
+if(interactive()) {
+    
+    # Set working directory
+    setwd('/Users/voitel/TRAINING/UW_Data_Science/UW_Data_Science_350/Week5/homework')
+    
+    # Import libraries
+    require(logging)
 
-# Open data file for reading
-df <- read.csv("ChicagoDiabetesData.csv")
+    # Setup Test Logger
+    basicConfig()
+    addHandler(writeToFile, file="testing.log", level='DEBUG')
 
-# Sum all values in all the columns excluding the first row (hadings)
-data_sums = apply(df[-1],2,sum)
+    # Open data file for reading
+    loginfo("Begin reading file")
+    df <- read.csv("ChicagoDiabetesData.csv")
+    loginfo("Completed reading file")
 
-# Extract x and y values from the list of column names extracted with names() function
-x_vals = data_sums[grepl('^Crude.Rate.[0-9]+$', names(data_sums), perl = TRUE)]
-y_vals = data_sums[grepl('^Hospitalizations.[0-9]+$', names(data_sums), perl = TRUE)]
+    # Sum all values in all the columns excluding the first row (hadings)
+    loginfo("Sum up values by column")
+    data_sums = apply(df[-1],2,sum)
 
-# Calculate consecutive differences in the values of X and Y to reduce the scale
-x_diffs = diff(x_vals)
-y_diffs = diff(y_vals)
+    # Extract x and y values from the list of column names extracted with names() function
+    loginfo("Extracting X-values - Crude Rate and Y-values - Hospitalizations")
+    x_vals = data_sums[grepl('^Crude.Rate.[0-9]+$', names(data_sums), perl = TRUE)]
+    y_vals = data_sums[grepl('^Hospitalizations.[0-9]+$', names(data_sums), perl = TRUE)]
+
+    # Delta's for X and Y - derived using diff() function
+    loginfo("Running diff() to reduce the scale of X and Y variables")
+    x_diffs = diff(x_vals)
+    y_diffs = diff(y_vals)
+    
+    # Produce simple graph (visual exploration of Curde Admission vs. Hospitalization)
+    simple_lm(x_vals, y_vals, "Crude Admission vs. Hospitalization")
+    
+    # Produce simple graph (visual exploration of Deltas of Cruda Admissin vs. Hospitalization)
+    simple_lm(x_diffs, y_diffs, "Deltas: Crude Admission vs Hospitalization")
+}
